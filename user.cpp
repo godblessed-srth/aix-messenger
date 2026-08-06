9e6b3bfc01381a5eb17b49e8845d86f5eb8412ee
godblessed-srth <timofejdenisov0@gmail.com>
2026-08-06 19:59:27 +0300

    add a global vector and id

--- user.cpp
+++ user.cpp
@@ -1,6 +1,9 @@
 #include "user.hpp"
 #include <iostream>
 #include <functional>
+// globals
+extern std::vector<User> g_users;
+extern int g_current_user_id = -1;
 // hash email
 std::string hashEmail(const std::string& email) {
     std::hash<std::string> hasher;
@@ -23,7 +26,7 @@ bool ValidEmail(const std::string& email) {
     return true;
 }
 // reg user
-int reg_user(const std::string& name, const std::string& email, std::vector<User>& users) {
+int reg_user(const std::string& name, const std::string& email) {
     if (name.empty() || email.empty()) {
         std::cout << RED << "[ ERR ] Name and email cannot be empty!\n" << RESET;
         return -1;
@@ -37,35 +40,35 @@ int reg_user(const std::string& name, const std::string& email, std::vector<User
     std::string new_hash = hashEmail(email);
     
     for (const auto& user : users) {
-        if (user.getHash() == new_hash) {
+        if (g_user.getHash() == new_hash) {
             std::cout << RED << "[ ERR ] Email " << email << "id already taken!\n" << RESET;
             return -1;
         }
     }
 
-    int next_id = users.empty() ? 0 : users.back().getId() + 1;
+    int next_id = users.empty() ? 0 : g_users.back().getId() + 1;
     User new_user(name, email, next_id);
-    users.push_back(new_user);
+    g_users.push_back(new_user);
     
     std::cout << GREEN << "[ OK ] Successfully registred! Welcome!\n" << RESET;
     return next_id;
 }
 // delete user
-void delete_user(std::vector<User>& users, int& current_user_id) {
-    if (current_user_id == -1) {
+void delete_user() {
+    if (g_current_user_id == -1) {
         std::cout << RED << "[ ERR ] No user to delete!\n" << RESET;
         return;
     }
-
-    int id_to_delete = current_user_id;
+    
+    int id_to_delete = g_current_user_id;
     for (auto it = users.begin(); it != users.end(); ++it) {
         if (it->getId() == id_to_delete) {
-            users.erase(it);
+            g_users.erase(it);
             std::cout << GREEN << "[ OK ] User deleted: " << id_to_delete << RESET << "\n";
-            current_user_id = -1;
+            g_current_user_id = -1;
             return;
         }
     }
-
+    
     std::cout << RED << "[ ERR ] User not found!\n" << RESET;
 }
