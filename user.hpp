4db49246ac6c844e0dcd37dca5fc430304f45336
godblessed-srth <timofejdenisov0@gmail.com>
2026-08-06 12:24:30 +0300

    Add getters and setters

--- user.hpp
+++ user.hpp
@@ -9,11 +9,24 @@ constexpr std::string_view RED = "\033[31m";
 constexpr std::string_view GREEN = "\033[32m";
 // user
 class User {
-    private:
-        std::string name = "Guest";
-        std::string email = "example@mail.com";
-        std::string hash_em = "";
-        int id = -1;  //unknown id
+private:
+    std::string name = "Guest";
+    std::string email = "example@mail.com";
+    std::string hash_em = "";
+    int id = -1;  //unknown id
+public:
+    User() = default;
+    User(const std::string& n, const std::string& e, int new_id) : name(e), email(e), id(new_id) {}
+    // getters
+    std::string getName() const { return name; }
+    std::string getEmail() const { return email; }
+    std::string getHash() const { return hash_em; }
+    int getId() const { return id; }
+    // setters
+    void setName(const std::string& n) { name = n; }
+    void setEmail(const std::string& e) { email = e; }
+    void setHash(const std::string& h) { hash_em = h; }
+    void setId(int new_id) { id = new_id; }
 };
 // reg user
 void reg_user(const std::string& name, const std::string& email, std::vector<User>& users);
