756964cc079db3fe2daa1e53c3525d471a70959d
godblessed-srth <timofejdenisov0@gmail.com>
2026-08-07 08:30:17 +0300

    Update message.cpp

--- message.cpp
+++ message.cpp
@@ -1,5 +1,10 @@
 include <iostream>
 #include "message.hpp"
+#include "user.hpp"
 
 int g_current_msg_id = -1;
 std::vector<Message> g_msgs;
+
+int send_msg(int author_id, int recipient_id, const std::string& msg) {
+  
+}
