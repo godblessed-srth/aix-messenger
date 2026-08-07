dc9d10e717c3e21bcfd0d384e6cb3a59ccb376fb
godblessed-srth <timofejdenisov0@gmail.com>
2026-08-07 08:10:31 +0300

    Big updates message.hpp: add struct and constructor

--- message.hpp
+++ message.hpp
@@ -1,10 +1,24 @@
 #pragma once
-#include <iostream>
+#include <ctime>
 #include <string>
 #include <vector>
 
 struct Message {
-  std::string author;
-  std::string recipient;
+  int msg_id = -1;
+  int author_id = -1;
+  int recipient_id = -1;
   std::string msg_text;
+  std::string timestamp;
+
+  Message() = default;
+  
+  Message(int author, int recipient, const std::string& msg) : author_id(author), recipient_id(recipient), msg_text(msg) {
+    auto now = std::time(nullptr);
+    char buff[20]
+    std::strftime(buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", std::localtime(&now));
+    timestamp = buff;
+  }
 }
+
+extern std::vector<Message> g_msgs;
+extern int g_msg_current_id = -1;
