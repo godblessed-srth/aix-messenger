80e8a5372b33c71e054c2fb349c65e5e93d877c2
godblessed-srth <timofejdenisov0@gmail.com>
2026-08-07 08:24:10 +0300

    small fixes

--- message.hpp
+++ message.hpp
@@ -2,7 +2,7 @@
 #include <ctime>
 #include <string>
 #include <vector>
-
+// struct
 struct Message {
   int msg_id = -1;
   int author_id = -1;
@@ -14,13 +14,18 @@ struct Message {
   
   Message(int author, int recipient, const std::string& msg) : author_id(author), recipient_id(recipient), msg_text(msg) {
     auto now = std::time(nullptr);
-    char buff[20]
-    std::strftime(buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", std::localtime(&now));
-    timestamp = buff;
+    char buff[20];
+    // localtime validate
+    auto time_ptr = std::localtime(&now);
+    if (time_ptr != nullptr) {
+      std::strftime(buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", time_ptr);
+      timestamp = buff;
+    } else {
+      timestamp = "1970-01-01 00:00:00";
   }
 }
-
+// global
 extern std::vector<Message> g_msgs;
-extern int g_msg_current_id = -1;
+extern int g_msg_current_id;
 
-int send_msg(int 
+int send_msg(int recipient_id, int author_id, const std::string& msg); 
